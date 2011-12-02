02288b32821059028b44f37c7553325ddb71e3c5
Christian Schudoma <schudoma@mpimp-golm.mpg.de>
2011-12-02 13:05:31 +0100

    Added testfile test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+import sys
+import math
+
+###
+def do_something():
+    return None 
+
+###
+def main(argv):
+    return None
+
+if __name__ == '__main__': main(sys.argv[1:])
